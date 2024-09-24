a212d91ae391a2c5f36d8a2960e73c65f7d26017
Danil Ermolin <thedanstrongny42@gmail.com>
2024-09-24 18:23:53 -0400

    Added different picture for Card component

--- community_board/src/components/Card.jsx
+++ community_board/src/components/Card.jsx
@@ -1,12 +1,11 @@
 import React from "react";
+import gamePic from './assets/the-elder-scrolls-v--skyrim-v5--19.png'
 
 const Card = () => {
     return(
         <div className="Card">
-            <img></img>
+            <img src={gamePic}  alt = "game picture" ></img>
             <h2>TES Skyrim</h2>
-                    
-
 
         </div>
     )
