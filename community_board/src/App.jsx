fa1d1fd538162ecbcb0e6049a202c97a158992a0
Danil Ermolin <thedanstrongny42@gmail.com>
2024-09-24 11:47:23 -0400

    Added component

--- community_board/src/App.jsx
+++ community_board/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Card from './components/Card';
 
 const App = () => {
 
   return (
     <div className="App">
       <h1>Trending Games Board</h1>
+      
     </div>
   )
 }
